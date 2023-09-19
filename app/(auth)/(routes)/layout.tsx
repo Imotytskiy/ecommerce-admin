2c60e84b58e60f6bceb53c2686a44dfee0f8647f
Imotytskiy <Imotytskiy@gmail.com>
2023-09-19 15:54:27 +0200

    add

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,5 +1,5 @@
-export function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex items-center justify-center h-full">{children}</div>
-  );
-}
+// export function AuthLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <div className="flex items-center justify-center h-full">{children}</div>
+//   );
+// }
